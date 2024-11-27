185074b1d2d4a161e092d55afbcf033ac12af8b3
huynhnhan15 <nhanhuynh959595@gmail.com>
2024-11-27 21:09:48 +0700

    baitap

--- src/ss1/bai_tap/ss1/bai2.java
+++ src/ss1/bai_tap/ss1/bai2.java
@@ -1,7 +1,14 @@
 package ss1.bai_tap.ss1;
 
+import java.util.Scanner;
+
 public class bai2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập vào số tiền cần chuyển đổi (USD): ");
+        double usd = Double.parseDouble(scanner.nextLine());
+        double vn = usd * 23000;
+        System.out.println("số tiền đã đổi là: " + vn + "VNĐ");
 
     }
 }
