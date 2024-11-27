6ee4589efdd00fb6fcc4f52b19a3372ed5d6add6
huynhnhan15 <nhanhuynh959595@gmail.com>
2024-11-27 20:51:45 +0700

    fsfs

---
+++ src/ss1/bai_tap/ss1/bai2.java
@@ -0,0 +1,7 @@
+package ss1.bai_tap.ss1;
+
+public class bai2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ss1/bai_tap/ss1/bai_1.java
@@ -0,0 +1,7 @@
+package ss1.bai_tap.ss1;
+
+public class bai_1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
